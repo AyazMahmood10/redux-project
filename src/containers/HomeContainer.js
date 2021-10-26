6c788aa59cd9a96f19a5a5c9ad1003cf18b86884
ayazmahmood10 <ayazmahmood0015@gmail.com>
2021-10-27 01:04:31 +0530

    Made connection between React and Redux

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,6 +1,16 @@
 //This container is a component which we will use to map the app.js file and the other home component. We won't directly import the HomeComponent to App so that we can import it to HomeContainer and from there we can transfer it to App and also we can connect it to redux
 
-import React from "react";
+import { connect } from "react-redux"; //to connect our React to Redux
 import HomeComponents from "../components/HomeComponents";
+import {addToCart} from '../service/actions/actions';
 
-export default HomeComponents
+const mapStateToProps = state => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({
+    addToCartHandler: data => dispatch(addToCart(data))
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeComponents)
--- src/service/actions/actions.js
+++ src/service/actions/actions.js
@@ -1,8 +1,8 @@
 import {ADD_TO_CART} from '../Constant'
 
-export const addToCart = (value) => {
+export const addToCart = (value) => { //This function tells the reducer what function is to be called. It contains two properties. one is type that will always be a string and the 2nd one is data received from containers.
     return {
-        type: ADD_TO_CART,
-        data: value
+        type: ADD_TO_CART,//type    
+        data: value //data
     }
 } 
